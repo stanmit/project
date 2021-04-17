d39515706bb75b21f9a8e83d81eee64bc8bdc7d8
stanmit <80504114+stanmit@users.noreply.github.com>
2021-04-17 12:33:32 +0530

    Delete step3.py

--- step3.py
+++
@@ -1,4 +0,0 @@
-from pyspark.sql import SparkSession
-spark=SparkSession.builder.appName("demo").getOrCreate()
-df10=spark.read.parquet('s3://project-mahesh/raw_zone/nse10')
-df10.coalesce(1).write.saveAsTable('nse10')
