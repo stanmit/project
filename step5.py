6f8c13b06ce4da756356e14f1cc7cb06c8748773
stanmit <80504114+stanmit@users.noreply.github.com>
2021-04-17 12:33:24 +0530

    Delete step5.py

--- step5.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-spark=SparkSession.builder.appName("demo").getOrCreate()
-dfi=spark.read.csv('/user/root/sqoop1/part-m-00000')
-dfj=spark.read.csv('/user/root/sqoop2/part-m-00000')
-df3=dfi.union(dfj)
-df3=df3.drop('_c15')
-df7=spark.read.parquet('s3://project-mahesh/raw_zone/nse70')
-df=df7.union(df3)
-df.coalesce(1).write.parquet('s3://project-mahesh/enriched')
